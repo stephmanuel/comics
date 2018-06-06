17a109223db174f10c36205b5fe53d5b2fb3e000
Raghu Betina <raghu@firstdraft.com>
2018-06-06 23:19:51 +0000

    Direct association between users and enrollments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :rosters,
+             :class_name => "Enrollment",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
